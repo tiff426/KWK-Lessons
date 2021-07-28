20727427080657627dcf40c92134baf71a094ad3
tiff426 <48851543+tiff426@users.noreply.github.com>
2021-07-28 11:40:51 -0400

    Add files via upload
    1st commit

---
+++ StoryBoardIntro/StoryBoardIntro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryBoardIntro
+//
+//  Created by Tiffany Liu on 7/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is a print statement - testing testing")
+    }
+
+
+}
+
