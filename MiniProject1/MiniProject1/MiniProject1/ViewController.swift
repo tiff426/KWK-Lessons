37f9cbc6e00658aee56f120df79241127aab6d2d
tiff426 <48851543+tiff426@users.noreply.github.com>
2021-07-28 21:51:30 -0400

    Updated

---
+++ MiniProject1/MiniProject1/MiniProject1/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  MiniProject1
+//
+//  Created by Tiffany Liu on 7/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var fact1: UILabel!
+    @IBOutlet weak var fact2: UILabel!
+    @IBOutlet weak var fact3: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    var clickCount = 0
+    
+    @IBAction func revealButton(_ sender: UIButton) {
+        clickCount+=1
+        if clickCount == 1 {
+            fact1.text = "I listen to a lot of indie pop but also kpop"
+        }
+        else if clickCount == 2 {
+            fact2.text = "I play ultimate frisbee"
+        }
+        else {
+            fact3.text = "I love kirby"
+        }
+    }
+    
+    
+}
+
