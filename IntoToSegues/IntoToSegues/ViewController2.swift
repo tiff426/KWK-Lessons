38a26b904d9cc582c2d2d84afadc83d9ce3643ca
tiff426 <48851543+tiff426@users.noreply.github.com>
2021-07-29 12:08:45 -0400

    Add files via upload

---
+++ IntoToSegues/IntoToSegues/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController3.swift
+//  IntoToSegues
+//
+//  Created by Tiffany Liu on 7/29/21.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    @IBOutlet weak var practiceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
