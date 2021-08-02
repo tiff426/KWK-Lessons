207c8ffc3a24fef99a91cac81b161a24222b6c04
tiff426 <48851543+tiff426@users.noreply.github.com>
2021-08-02 10:27:19 -0400

    Add files via upload

---
+++ ToDoListProject/ToDoList/ToDoList/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Tiffany Liu on 8/2/21.
+//
+
+import UIKit
+
+class ToDoClass {
+    
+    var description = ""
+    var important = false
+    
+}
